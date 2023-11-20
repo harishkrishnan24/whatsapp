1c99cd988523c1ba15d333c619293d7e47b4f782
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-11-20 19:42:14 +0530

    add db config

--- whatsapp_be/src/index.js
+++ whatsapp_be/src/index.js
@@ -1,7 +1,22 @@
+import mongoose from "mongoose";
 import app from "./app.js";
 import logger from "./configs/logger.config.js";
 
 const PORT = process.env.PORT || 8000;
+const { DATABASE_URL } = process.env;
+
+mongoose.connection.on("error", (err) => {
+  logger.error(`MongoDB connection error: ${err}`);
+  process.exit(1);
+});
+
+mongoose.connect(DATABASE_URL).then(() => {
+  logger.info("Connected to MongoDB...");
+});
+
+if (process.env.NODE_ENV !== "production") {
+  mongoose.set("debug", true);
+}
 
 let server = app.listen(PORT, () => {
   logger.info(`Server is listening at ${PORT}...`);
