2730fe1481a30db14cdfe613c8382042f5a61dd3
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-06-05 21:28:19 +0530

    [BE] configure middlewares

--- whatsapp_be/src/app.js
+++ whatsapp_be/src/app.js
@@ -1,6 +1,29 @@
 import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import expressFileupload from "express-fileupload";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(mongoSanitize());
+app.use(cookieParser());
+app.use(compression());
+app.use(expressFileupload({ useTempFiles: true }));
+app.use(cors());
 
 app.get("/test", (req, res) => {
   res.send("Hello from server");
--- whatsapp_be/src/index.js
+++ whatsapp_be/src/index.js
@@ -1,9 +1,8 @@
-import dotenv from "dotenv";
 import app from "./app.js";
 
-dotenv.config();
-
 const PORT = process.env.PORT || 8000;
+
+console.log(process.env.NODE_ENV);
 
 app.listen(PORT, () => {
   console.log(`server is listening at ${PORT}`);
