d8005a4270e14b65dced001335e2b03e60995073
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-06-16 21:53:21 +0530

    handle unhandled errors for graceful shutdown

--- whatsapp_be/src/index.js
+++ whatsapp_be/src/index.js
@@ -3,8 +3,26 @@ import logger from "./config/logger.config.js";
 
 const PORT = process.env.PORT || 8000;
 
-console.log(process.env.NODE_ENV);
+logger.info("NODE_ENV: " + process.env.NODE_ENV);
 
-app.listen(PORT, () => {
-  logger.info(`server is listening at ${PORT}`);
+const server = app.listen(PORT, () => {
+    logger.info(`server is listening at ${PORT}`);
 });
+
+const exitHandler = () => {
+    if (server) {
+        logger.info('Server closed.');
+        process.exit(1);
+    } else {
+        process.exit(1);
+    }
+}
+
+const unexpectedErrorHandler = (error) => {
+    logger.error(error);
+    exitHandler();
+}
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+process.on('SIGTERM', unexpectedErrorHandler);
