d3271654974f791e0ded6c84a0513960bd070256
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-06-16 22:04:02 +0530

    common error handling

--- whatsapp_be/src/app.js
+++ whatsapp_be/src/app.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import expressFileupload from "express-fileupload";
 import cors from "cors";
+import createHttpError from "http-errors";
 
 dotenv.config();
 
@@ -27,6 +28,18 @@ app.use(cors());
 
 app.get("/test", (req, res) => {
   res.send("Hello from server");
+});
+
+app.use(async (req, res, next) => {
+  next(createHttpError.NotFound("This route does not exist"));
+});
+
+// error handling
+app.use(async (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: { status: err.status || 500, message: err.message },
+  });
 });
 
 export default app;
