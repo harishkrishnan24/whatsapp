921e4a907e606728e516db38594f60de2c64469e
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-06-19 20:59:01 +0530

    add redux store config

---
+++ whatsapp_fe/src/app/store.js
@@ -0,0 +1,32 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import userSlice from "../features/userSlice";
+import { createFilter } from "redux-persist-transform-filter";
+
+const saveUserOnlyFilter = createFilter("user", ["user"]);
+
+const persistConfig = {
+  key: "user",
+  storage,
+  whitelist: ["user"],
+  transforms: [saveUserOnlyFilter],
+};
+
+const rootReducer = combineReducers({
+  user: userSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => {
+    getDefaultMiddleware({
+      serializableCheck: false,
+    });
+  },
+  devTools: true,
+});
+
+export const persistor = persistStore(store);
