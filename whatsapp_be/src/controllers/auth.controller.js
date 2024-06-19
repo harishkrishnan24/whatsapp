7263a248f92fcd8bf6d45b013463cb1b7af830ce
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-06-19 21:49:43 +0530

    user signup

--- whatsapp_be/src/controllers/auth.controller.js
+++ whatsapp_be/src/controllers/auth.controller.js
@@ -1,5 +1,44 @@
+import { createUser } from "../services/auth.service.js";
+import { generateToken } from "../services/token.service.js";
+
 export const register = async (req, res, next) => {
   try {
+    const { name, email, picture, status, password } = req.body;
+    const newUser = await createUser({
+      name,
+      email,
+      picture,
+      status,
+      password,
+    });
+    const access_token = await generateToken(
+      { userId: newUser._id },
+      "1d",
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    const refresh_token = await generateToken(
+      { userId: newUser._id },
+      "30d",
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    res.cookie("refreshtoken", refresh_token, {
+      httpOnly: true,
+      path: "/api/v1/auth/refreshtoken",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+
+    res.json({
+      message: "register success",
+      access_token,
+      user: {
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        picture: newUser.picture,
+        status: newUser.status,
+      },
+    });
   } catch (error) {
     next(error);
   }
---
+++ whatsapp_be/src/utils/token.util.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import logger from "../config/logger.config.js";
+
+export const sign = async (payload, expiresIn, secret) => {
+  return new Promise((resolve, reject) => {
+    jwt.sign(
+      payload,
+      secret,
+      {
+        expiresIn,
+      },
+      (error, token) => {
+        if (error) {
+          logger.error(error);
+          reject(error);
+        } else {
+          resolve(token);
+        }
+      }
+    );
+  });
+};
